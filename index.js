5c2408706e6ae162d42db4cff2e0a44eb9dc5b82
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-13 14:04:59 +0000

    solution

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 /*
 Write a function that reverses a string. The input string is given as an array of characters s.
 */
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let start = 0
+    let end = s.length-1
+    while (start <= end){
+        // console.log(start++,end--)
+        [s[start++],s[end--]] = [s[end],s[start]]
+    }
+};
