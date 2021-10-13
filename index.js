3b68aac8596085a78694f38600946108b7b0fba6
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-13 15:43:20 +0000

    cleanup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Write a function that reverses a string. The input string is given as an array of characters s.
+344 Write a function that reverses a string. The input string is given as an array of characters s.
 */
 
 /**
